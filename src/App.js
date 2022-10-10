6dd36b2848fe27bf8631f2fe357f017b79525762
Sathindu Kavneth <sathindukavneth@gmail.com>
2022-10-09 20:06:03 -0400

    React Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sample <code>React</code> Project
+        </p>
+        <a
+          className="App-link"
+          href="https://sathindu.me"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Main Page
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
